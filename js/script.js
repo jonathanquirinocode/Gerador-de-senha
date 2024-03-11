5f982281808417777053ad5f48334adf75ad04b8
Jonathan Quirino <jonathanquirino30@gmail.com>
2024-03-11 16:15:28 -0300

    finalizando o gerador de senhas
    script do gerador de senhas aleatório finalizado.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //seleção de elementos
 const generatePasswordBtn = document.querySelector("#generate-password");
-const generatedPasswordBtn = document.querySelector("#generated-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
 //funções
 
 const getLetterLowCase = () => {
@@ -20,7 +20,40 @@ const getSymbols = () => {
     return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
+const generatePassword = (getLetterLowCase, getLetterUpperCase, getNumber, getSymbols) => {
+    
+    let password = "";
+
+    const passwordLength = 10;
+
+    const generators = [
+        getLetterLowCase,
+        getLetterUpperCase,
+        getNumber, getSymbols
+    ];
+
+    for (i = 0; i < passwordLength; i = i + generators.length){
+        generators.forEach(() => {
+            const randomValue = 
+                generators[Math.floor(Math.random() * generators.length)]();
+
+            password += randomValue;
+        });
+    };
+
+    password = password.slice(0, passwordLength);
+
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector("h4").innerText = password;
+
+};
+
+
 //eventos
 generatePasswordBtn.addEventListener("click", () =>{
-
+    generatePassword(
+        getLetterLowCase,
+        getLetterUpperCase,
+        getNumber, getSymbols
+    )
 });
